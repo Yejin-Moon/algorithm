38bfe7248b44ab0939256cb1a6c0dc1eb3c69dcd
문예진 <dpwls991226@naver.com>
2021-12-11 01:14:25 +0900

    Git test2 & buble sort

--- Git_test.py
+++ Git_test.py
@@ -9,5 +9,13 @@ def insert_sort(arr):
                     j=j-1
     return arr
 
+def buble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1], arr[j]
+    return arr
+
 arr = [5,4,3,2,1]
-print(insert_sort(arr))        
+#print(insert_sort(arr))   
+print(buble_sort(arr))     
