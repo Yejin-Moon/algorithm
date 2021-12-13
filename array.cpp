9c60cd8f98daabae77a521f1b1e69c7bff304f57
문예진 <dpwls991226@naver.com>
2021-12-13 18:44:51 +0900

    c++ env setting

---
+++ array.cpp
@@ -0,0 +1,11 @@
+#include <array>
+#include <iostream>
+
+int main()
+{
+    std::array<int, 10> arr1;
+    arr1[0] = 1;
+    std::cout << "arr1 배열의 원소: " <<arr1[0]<<std::endl;
+    std::cout<<"Hello";
+}
+
