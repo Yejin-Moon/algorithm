358ea0bbb5df05a36e419c9f54aade4771a95d3e
문예진 <dpwls991226@naver.com>
2021-12-21 21:33:32 +0900

    find prime number with a vector

---
+++ C++/vector_primes.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool is_prime(int num){
+    if(num<2){
+        return false;
+    }
+    for(int i = 2; i < num; i++){
+        if(num %i ==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> primes(int low, int up){
+    vector<int> vec;
+
+    for(int i = low; i<=up; i++){
+        if(is_prime(i)){
+            vec.push_back(i);
+        }
+    }
+    return vec;
+}
+
+int main()
+{
+    int lower = 1, upper = 100;
+
+    vector<int> v_prime = primes(lower, upper);
+    for(int elem: v_prime){
+        cout<<elem <<'\n';
+    }
+    cout<<endl;
+
+    return 0;
+}
