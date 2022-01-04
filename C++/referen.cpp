4b9bea47060e0ce35ad879cb33dcdf613a5ee864
문예진 <dpwls991226@naver.com>
2022-01-04 23:25:20 +0900

    reference

--- C++/referen.cpp
+++ C++/referen.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main()
 {
-    int num1 = 1020;
-    int &num2 = num1;
+    int arr[3]={1,3,5};
+    int &ref1 = arr[0];
+    int &ref2 = arr[1];
+    int &ref3 = arr[2];
 
-    num2 = 3047;
-    cout<<"VAL: "<<num1<<endl;
-    cout<<"REF: "<<num2<<endl;
-
-    cout<<"VAL: "<<&num1<<endl;
-    cout<<"REFL "<<&num2<<endl;
+    cout<<ref1<<endl;
+    cout<<ref2<<endl;
+    cout<<ref3<<endl;
     return 0;
 }
