3ef9a89c350309cb624ef20e11b9ef4cd77dd6c6
문예진 <dpwls991226@naver.com>
2022-01-09 22:32:13 +0900

    Call by Reference - Swap pointer

--- C++/callbyreference.cpp
+++ C++/callbyreference.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void plusone(int &a)
+void swappointer(int *&ptr1, int *&ptr2)
 {
-    a = a+1;
-    return;
-}
-
-void changepm(int &b)
-{
-    b = b*-1;
-    return;
+    int *tmp = ptr1;
+    ptr1 = ptr2;
+    ptr2 = tmp;
 }
 
 int main()
 {
-    int v1, v2;
-    v1 = 2;
-    v2 = -3;
-    plusone(v1);
-    changepm(v2);
-    cout<<v1<<endl;
-    cout<<v2<<endl;
+    int num1 = 5;
+    int *ptr1 = &num1;
+    int num2 = 10;
+    int *ptr2 = &num2;
 
+    cout<<"before change"<<endl;
+    cout<<*ptr1<<endl;
+    cout<<*ptr2<<endl;
+
+    swappointer(ptr1, ptr2);
+
+    cout<<"after change"<<endl;
+    cout<<*ptr1<<endl;
+    cout<<*ptr2<<endl;
     return 0;
 }
