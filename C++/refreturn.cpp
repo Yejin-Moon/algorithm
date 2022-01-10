3dcf39589de46ef9d1da79abecc36f1dbfc0a975
문예진 <dpwls991226@naver.com>
2022-01-10 22:09:23 +0900

    reference return2

--- C++/refreturn.cpp
+++ C++/refreturn.cpp
@@ -10,10 +10,10 @@ int& refretfunc(int &ref)
 int main()
 {
     int num1 = 1;
-    int &num2 = refretfunc(num1);
+    int num2 = refretfunc(num1);
 
-    num1++;
-    num2++;
+    num1+=1;
+    num2+=100;
     cout<<"num1 : "<<num1<<endl;
     cout<<"num2 : "<<num2<<endl;
     return 0;
