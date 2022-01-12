1c885ed8571955ea52aca4c10f0ca9ff9c95f440
문예진 <dpwls991226@naver.com>
2022-01-12 17:21:17 +0900

    new/delete

--- C++/malfree.cpp
+++ C++/malfree.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <string.h>
-#include <stdlib.h>
 using namespace std;
 
 char * MakeStrAdr(int len)
 {
-    char * str = (char*)malloc(sizeof(char)*len);
+    char * str= new char[len];
     return str;
 }
 
 int main()
 {
     char * str = MakeStrAdr(20);
-    strcpy(str, "I am so happy");
-    cout<<str<<endl;
-    free(str);
+    strcpy(str, "I am happy");
+    cout << str<< endl;
+    delete []str;
     return 0;
 }
