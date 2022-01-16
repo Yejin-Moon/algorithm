57fa6e6898a5755133524302d9b48455d2598275
문예진 <dpwls991226@naver.com>
2022-01-16 17:43:22 +0900

    Racing car - function add

--- C++/RacingCar.cpp
+++ C++/RacingCar.cpp
@@ -12,56 +12,56 @@ struct Car
     char gamerID[ID_LEN];
     int fuelGauge;
     int curSpeed;
+
+    void ShowCarState(const Car &car)
+    {
+        cout<<"Owner ID: "<<car.gamerID<<endl;
+        cout<<"Amount of fuel: "<<car.fuelGauge<<"%"<<endl;
+        cout<<"Current Speed: "<<car.curSpeed<<"km/s"<<endl<<endl;
+    }
+
+    void Accel(Car &car)
+    {
+        if(car.fuelGauge<=0)
+        {
+            return;
+        }
+        else
+        {
+            car.fuelGauge-=FUEL_STEP;
+        }
+
+        if(car.curSpeed+ACC_STEP>=MAX_SPD)
+        {
+            car.curSpeed=MAX_SPD;
+            return;
+        }
+        car.curSpeed+-ACC_STEP;
+    }
+
+    void Break(Car &car)
+    {
+        if(car.curSpeed<BRK_STEP)
+        {
+            car.curSpeed=0;
+            return;
+        }
+        car.curSpeed-=BRK_STEP;
+    }
 };
-
-void ShowCarState(const Car &car)
-{
-    cout<<"Owner ID: "<<car.gamerID<<endl;
-    cout<<"Amount of fuel: "<<car.fuelGauge<<"%"<<endl;
-    cout<<"Current Speed: "<<car.curSpeed<<"km/s"<<endl<<endl;
-}
-
-void Accel(Car &car)
-{
-    if(car.fuelGauge<=0)
-    {
-        return;
-    }
-    else
-    {
-        car.fuelGauge-=FUEL_STEP;
-    }
-
-    if(car.curSpeed+ACC_STEP>=MAX_SPD)
-    {
-        car.curSpeed=MAX_SPD;
-        return;
-    }
-    car.curSpeed+-ACC_STEP;
-}
-
-void Break(Car &car)
-{
-    if(car.curSpeed<BRK_STEP)
-    {
-        car.curSpeed=0;
-        return;
-    }
-    car.curSpeed-=BRK_STEP;
-}
 
 int main()
 {
     Car run99 = {"run99",100,20};
-    Accel(run99);
-    Accel(run99);
-    ShowCarState(run99);
-    Break(run99);
-    ShowCarState(run99);
+    run99.Accel(run99);
+    run99.Accel(run99);
+    run99.ShowCarState(run99);
+    run99.Break(run99);
+    run99.ShowCarState(run99);
 
     Car sped77={"sped77",100,0};
-    Accel(sped77);
-    Break(sped77);
-    ShowCarState(sped77);
+    sped77.Accel(sped77);
+    sped77.Break(sped77);
+    sped77.ShowCarState(sped77);
     return 0;
 }
