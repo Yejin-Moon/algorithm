5cdb8547522010139898f78b3b07a1b290a10431
문예진 <dpwls991226@naver.com>
2022-02-06 20:50:36 +0900

    Circle, ring class

--- C++/Class/Circleclass.cpp
+++ C++/Class/Circleclass.cpp
@@ -15,5 +15,50 @@ public:
     {
         cout<<"["<<xpos<<", "<<ypos<<"]"<<endl;
     }
-
 };
+
+class Circle
+{
+private:
+    int rad;
+    Point center;
+public:
+    void Init(int x, int y, int r)
+    {
+        rad = r;
+        center.Init(x,y);
+    }
+    void ShowCircleInfo() const
+    {
+        cout<<"radius : "<<rad<<endl;
+        center.ShowPointInfo();
+    }
+};
+
+class Ring
+{
+private:
+    Circle inCircle;
+    Circle outCircle;
+public:
+    void Init(int inX, int inY, int inR, int outX, int outY, int outR)
+    {
+        inCircle.Init(inX, inY, inR);
+        outCircle.Init(outX, outY, outR);
+    }
+    void ShowRingInfo() const
+    {
+        cout<<"Inner Circle info..."<<endl;
+        inCircle.ShowCircleInfo();
+        cout<<"Outter Circle info..."<<endl;
+        outCircle.ShowCircleInfo();
+    }
+};
+
+int main()
+{
+    Ring ring;
+    ring.Init(1,1,4,2,2,9);
+    ring.ShowRingInfo();
+    return 0;
+}
