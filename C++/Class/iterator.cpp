6b93608b7a5f1be7198d21b4ac518d42eec9f0c2
문예진 <dpwls991226@naver.com>
2022-03-03 23:38:48 +0900

    Iterator

---
+++ C++/Class/iterator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <forward_list>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<string> vec = {
+        "Lewis", "Lewis", "Nico", "Sebastian", "Lewis","Sebastian",
+        "Sebastian", "Sebastian", "Fernando"
+    };
+
+    auto it = vec.begin();
+    cout<<"Most Recent Winner: " <<*it<<endl;
+    it+=8;
+    cout<<"Winner 8 years ago: "<<*it<<endl;
+    advance(it,-3);
+    cout<<"Winnder 3 years later: "<<*it<<endl;
+
+    forward_list<string> fwd(vec.begin(), vec.end());
+
+    auto it1 = fwd.begin();
+    cout<<"Most resent winner: "<<*it1<<endl;
+
+    advance(it1, 5);
+    cout<<"Winner 5 years ago: "<<*it1<<endl;
+}
