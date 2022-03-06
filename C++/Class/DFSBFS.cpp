0e68d73ca50dbb91eb31430f7e94c729246b8d81
문예진 <dpwls991226@naver.com>
2022-03-06 23:22:00 +0900

    DFS/BFS complete

--- C++/Class/DFSBFS.cpp
+++ C++/Class/DFSBFS.cpp
@@ -3,61 +3,64 @@
 #include <vector>
 using namespace std;
 
-int number = 9;
-int visit[9];
-int visit2[9];
-vector<int> a[10];
+int visit[1001];
+int visit2[1001];
+vector<int> a[1001];
 
 void dfs(int start)
 {
-    if(visit[start])
-    {
-        return;
-    }
+	if (visit[start])
+	{
+		return;
+	}
 
-    visit[start] = true;
-    printf("%d ",start);
+	visit[start] = true;
+	printf("%d ", start);
 
-    for(int i = 0; i<a[start].size(); i++)
-    {
-        int x = a[start][i];
-        dfs(x);
-    }
+	for (int i = 0; i < a[start].size(); i++)
+	{
+		int x = a[start][i];
+		dfs(x);
+	}
 }
 
-void bfs(int start){
-    queue<int> q;
-    q.push(start);
-    visit2[start] = true;
+void bfs(int start) {
+	queue<int> q;
+	q.push(start);
+	visit2[start] = true;
 
-    while(!q.empty()){
-        int x = q.front();
-        q.pop();
-        printf("%d ",x);
-        for(int i = 0; i<a[x].size(); i++){
-            int y = a[x][i];
-            if(!visit2[y]){
-                q.push(y);
-                visit2[y] = true;
-            }
-        }
-    }
+	while (!q.empty()) {
+		int x = q.front();
+		q.pop();
+		printf("%d ", x);
+		for (int i = 0; i < a[x].size(); i++) {
+			int y = a[x][i];
+			if (!visit2[y]) {
+				q.push(y);
+				visit2[y] = true;
+			}
+		}
+	}
 }
 
 int main()
 {
-    int N, M, S;
-    scanf("%d %d %d",&N,&M,&S);
-    
-    int st, en;
-    for(int i = 0; i<M;i++)
-    {
-        scanf("%d %d",&st,&en);
-        a[st].push_back(en);
-        a[en].push_back(st);
-    }
+	int N, M, S;
+	scanf("%d %d %d", &N, &M, &S);
 
-    dfs(S);
-    printf("\n");
-    bfs(S);
+	int st, en;
+	for (int i = 0; i < M; i++)
+	{
+		scanf("%d %d", &st, &en);
+		a[st].push_back(en);
+		a[en].push_back(st);
+	}
+
+	for (int i = 0; i <= N; i++)
+	{
+		sort(a[i].begin(), a[i].end());
+	}
+	dfs(S);
+	printf("\n");
+	bfs(S);
 }
