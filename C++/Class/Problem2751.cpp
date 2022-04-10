987b76bdaf6853903d6705f5bd5d0acdff9059e0
문예진 <dpwls991226@naver.com>
2022-04-10 21:36:02 +0900

    Problem 2751 - Sorting

---
+++ C++/Class/Problem2751.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n,t;
+    vector<int> a;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cin>>t;
+        a.push_back(t);
+    }
+    sort(a.begin(),a.end());
+    for(int i=0; i<n; i++)
+        cout<<a[i]<<'\n';
+}
