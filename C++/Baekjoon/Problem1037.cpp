dcfd9cca7a7b5bb74a01ea815f538955f1704c8b
문예진 <dpwls991226@naver.com>
2022-04-12 18:32:26 +0900

    Problem 1037 - divisor

---
+++ C++/Baekjoon/Problem1037.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[51];
+    int small = arr[0], big = 1;
+
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<<arr[0]*arr[n-1];
+}
