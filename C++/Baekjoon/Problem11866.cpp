17093d458cc31ef67cb4c4aa92cdea70529b0b4e
문예진 <dpwls991226@naver.com>
2022-04-18 15:38:52 +0900

    Problem 11866 - 요세푸스 순열

---
+++ C++/Baekjoon/Problem11866.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    
+    int n,k;
+    cin>>n>>k;
+    queue<int> q;
+
+    for(int i=1; i<=n; i++)
+    {
+        q.push(i);
+    }
+    int tmp;
+    cout<<"<";
+    while(q.size()>1)
+    {
+        for(int i=1; i<k; i++)
+    {
+        tmp = q.front();
+        q.pop();
+        q.push(tmp);
+    }
+    cout<<q.front()<<", ";
+    q.pop();
+    }
+    cout<<q.front()<<">"<<'\n';
+}
