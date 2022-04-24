dc9a1b4b6b971e6bfbb1452bd56e4547da668d05
문예진 <dpwls991226@naver.com>
2022-04-24 18:35:56 +0900

    Problem 2579 - 계단오르기 (다이나믹 프로그래밍)

--- C++/Baekjoon/Problem2579.cpp
+++ C++/Baekjoon/Problem2579.cpp
@@ -1,49 +1,28 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 using namespace std;
+
+int stair[301];
+int dp[301];
+
 
 int main()
 {
     int n;
-    vector<int> v;
     cin>>n;
     for(int i=0; i<n; i++)
     {
-        int a;
-        cin>>a;
-        v.push_back(a);
+        cin>>stair[i];
     }
 
-    int cnt=v[n-1];
-    bool flag = true;
-    bool flag2 = false;
-    int dp = v[n-1];
-    
-    for(int i=n-2; i>=0; i=i-2)
+    dp[0] = stair[0];
+    dp[1] = max(stair[0] + stair[1], stair[1]);
+    dp[2] = max(stair[0]+stair[2], stair[1]+stair[2]);
+
+    for(int i=3; i<n; i++)
     {
-        cout<<cnt<<'\n';
-        cout<<i<<'\n';
-        if(i==0 && flag2 == true) break;
-        else if(i==0 && flag2 == false && flag == true) 
-        {
-            cnt=cnt+v[i];
-            break;
-        }
-        else if(i==0 && flag2 == false && flag == false) break; 
-        if(flag==false) 
-        {
-            cnt=cnt+v[i]+v[i-1];
-            flag = true;
-        }
-        else
-        {
-            int one = max(v[i],v[i-1]);
-            if(v[i]==v[i-1]) one = v[i];
-            cnt = cnt+one;
-            if(one==v[i]) flag = false;
-            else flag = true;
-        }
+        dp[i] = max(dp[i-2]+stair[i], dp[i-3]+stair[i-1]+stair[i]);
     }
-    cout<<cnt<<'\n';
+    cout<<dp[n-1]<<'\n';
+    return 0;
 }
