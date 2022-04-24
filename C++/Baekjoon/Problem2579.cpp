39e29b923bb7e1c920c5cfb5819430009ed923fe
문예진 <dpwls991226@naver.com>
2022-04-24 17:26:38 +0900

    Problem 2579 - 계단오르기 미완

--- C++/Baekjoon/Problem2579.cpp
+++ C++/Baekjoon/Problem2579.cpp
@@ -8,7 +8,6 @@ int main()
     int n;
     vector<int> v;
     cin>>n;
-
     for(int i=0; i<n; i++)
     {
         int a;
@@ -17,12 +16,34 @@ int main()
     }
 
     int cnt=v[n-1];
-    int one,two;
-    for(int i=n-2; i>=0; i=-3)
+    bool flag = true;
+    bool flag2 = false;
+    int dp = v[n-1];
+    
+    for(int i=n-2; i>=0; i=i-2)
     {
-        sort(v[i],v[i-2]);
-        cout<<v[i]<<' '<<v[i-1]<<' '<<v[i-2];
-
+        cout<<cnt<<'\n';
+        cout<<i<<'\n';
+        if(i==0 && flag2 == true) break;
+        else if(i==0 && flag2 == false && flag == true) 
+        {
+            cnt=cnt+v[i];
+            break;
+        }
+        else if(i==0 && flag2 == false && flag == false) break; 
+        if(flag==false) 
+        {
+            cnt=cnt+v[i]+v[i-1];
+            flag = true;
+        }
+        else
+        {
+            int one = max(v[i],v[i-1]);
+            if(v[i]==v[i-1]) one = v[i];
+            cnt = cnt+one;
+            if(one==v[i]) flag = false;
+            else flag = true;
+        }
     }
-    //cout<<cnt<<'\n';
+    cout<<cnt<<'\n';
 }
