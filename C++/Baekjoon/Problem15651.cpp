8a6a0a22bdc388970d00b6079b2664cfe6b3d2ad
문예진 <dpwls991226@naver.com>
2022-04-30 22:49:45 +0900

    Problem 15651 - 순열

---
+++ C++/Baekjoon/Problem15651.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MAX 8
+
+int arr[MAX];
+bool sel[MAX];
+vector<int> v;
+
+void print()
+{
+    for(int i=0; i< v.size(); i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    cout<<'\n';
+}
+
+void DFS(int cnt, int n, int m)
+{
+    if(cnt == m)
+    {
+        print();
+        return;
+    }
+    for(int i=0; i<n; i++)
+    {
+        //if(sel[i] == true) continue;
+        sel[i] = true;
+        v.push_back(arr[i]);
+        DFS(cnt+1,n,m);
+        v.pop_back();
+        sel[i] = false;
+    }
+}
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    for(int i=0; i<n; i++)
+    {
+        arr[i] = i+1;
+    }
+    DFS(0,n,m);
+}
