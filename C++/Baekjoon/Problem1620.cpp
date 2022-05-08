e268597ad7a74f66f797f3515146304e59cc62c8
문예진 <dpwls991226@naver.com>
2022-05-08 23:10:43 +0900

    Problem 1620 - 포켓몬 마스터(이분탐색으로 미완)

--- C++/Baekjoon/Problem1620.cpp
+++ C++/Baekjoon/Problem1620.cpp
@@ -1,49 +1,65 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
-
-struct Poket
-{
-    int n;
-    string name;
-};
 
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    Poket *p = new Poket[a+1];
-    for(int i=1; i<=a; i++)
+    //ios_base::sync_with_stdio(false);
+	//cin.tie(NULL);
+	//cout.tie(NULL);
+
+    int n,m;
+    cin>>n>>m;
+    vector<string> v1;
+    vector<pair<string, int> >v2;
+    v1.push_back("");
+
+    for(int i=1; i<=n; i++)
     {
-        p[i].n = i;
-        cin>>p[i].name;
+        string s;
+        cin>>s;
+        v1.push_back(s);
+        v2.push_back({s,i});
     }
 
-    //string str;
+    sort(v2.begin(),v2.end());
+
     char str[21];
-    for(int i=0; i<b; i++)
+    for(int i=0; i<m; i++)
     {
         cin>>str;
-        if(str[0]>'0' && str[0]<'9')
+        if(str[0]>'0' && str[0] <'9')
         {
-            int an;
-            sscanf(str,"%d", &an);
-            for(int j=1; j<=a; j++)
+            int a;
+            sscanf(str,"%d",&a);
+            cout<<v1[a]<<'\n';
+        }
+        else
+        {
+            int mid = 0;
+            int low = 0;
+            int high = v2.size()-1;
+
+            while(low<=high)
             {
-                if(p[j].n == an)
+                mid = (low+high)/2;
+                if(v2[mid].first==string(str))
                 {
-                    cout<<p[j].name<<'\n';
+                    cout<<v2[mid].second<<'\n';
                     break;
+                }
+                else if(v2[mid].first<string(str))
+                {
+                    low = mid+1;
+                }
+                else
+                {
+                    high = mid-1;
                 }
             }
         }
-        for(int j=1; j<=a; j++)
-        {
-            if(p[j].name == str)
-            {
-                cout<<p[j].n<<'\n';
-                break;
-            }
-        }
     }
+
 }
