0ba2f7953478d3abcfc2b1d65405eac949b93e41
문예진 <dpwls991226@naver.com>
2022-05-08 23:30:38 +0900

    Problem 1620 - 포켓몬 마스터(map 이용 미완)

--- C++/Baekjoon/Problem1620.cpp
+++ C++/Baekjoon/Problem1620.cpp
@@ -1,65 +1,41 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <algorithm>
+#include <cstdio>
+#include <map>
+#include <cstdlib>
 using namespace std;
+
+string m2[100001];
 
 int main()
 {
-    //ios_base::sync_with_stdio(false);
-	//cin.tie(NULL);
-	//cout.tie(NULL);
-
     int n,m;
     cin>>n>>m;
-    vector<string> v1;
-    vector<pair<string, int> >v2;
-    v1.push_back("");
-
-    for(int i=1; i<=n; i++)
+    char str[21];
+    string s;
+    map<string, int> m1;
+    for(int i=0; i<n; i++)
     {
-        string s;
         cin>>s;
-        v1.push_back(s);
-        v2.push_back({s,i});
+        //string s = str;
+        m1.insert(pair<string, int>(s,i));
+        m2[i]=s;
     }
 
-    sort(v2.begin(),v2.end());
-
-    char str[21];
     for(int i=0; i<m; i++)
     {
         cin>>str;
-        if(str[0]>'0' && str[0] <'9')
+        if(str[0]>'0' && str[0]<'9')
         {
             int a;
             sscanf(str,"%d",&a);
-            cout<<v1[a]<<'\n';
+            cout<<m2[a-1]<<'\n';
         }
         else
         {
-            int mid = 0;
-            int low = 0;
-            int high = v2.size()-1;
-
-            while(low<=high)
-            {
-                mid = (low+high)/2;
-                if(v2[mid].first==string(str))
-                {
-                    cout<<v2[mid].second<<'\n';
-                    break;
-                }
-                else if(v2[mid].first<string(str))
-                {
-                    low = mid+1;
-                }
-                else
-                {
-                    high = mid-1;
-                }
-            }
+            string s2 = str;
+            cout<<m1[s2]+1<<'\n';
         }
     }
-
+    return 0;
 }
