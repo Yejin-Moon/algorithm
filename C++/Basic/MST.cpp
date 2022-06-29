8f0935711405c1814131e9937b28375c53fed185
문예진 <dpwls991226@naver.com>
2022-06-29 12:07:25 +0900

    MST

--- C++/Basic/MST.cpp
+++ C++/Basic/MST.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#define SZ 8
 using namespace std;
 
-int getparent(int set[], int x)
-{
-    if(set[x]==x) return x;
-    return set[x] = getparent(set,set[x]);
+int getparent(int cycle[], int x)
+{//cycledetection 배열에서 부모가 누군지 가져옴
+    if(cycle[x]==x) return x;
+    return cycle[x] = getparent(cycle,cycle[x]);
 }
 
-void unionparent(int set[],int a, int b)
-{
-    a=getparent(set,a);
-    b=getparent(set,b);
-    //숫자가 작은 부모로 병합
-    if(a<b) set[b]=a;
-    else set[a]=b;
+void unionparent(int cycle[],int a, int b)
+{//연결되었다는 의미로 부모를 같게 해준다.
+    a=getparent(cycle,a);
+    b=getparent(cycle,b);
+    //숫자가 앞서는 부모를 기준으로 연결
+    if(a<b) cycle[b]=a;
+    else cycle[a]=b;
 }
 
-int find(int set[], int a, int b)
-{
-    a=getparent(set, a);
-    b=getparent(set, b);
+int find(int cycle[], int a, int b)
+{//부모가 같은지 확인
+    a=getparent(cycle, a);
+    b=getparent(cycle, b);
     if(a==b) return 1;
     else return 0;
 }
@@ -29,24 +30,22 @@ int find(int set[], int a, int b)
 class Edge
 {
 public:
-    int node[2];
-    int distance;
-    Edge(int a, int b, int distance)
+    int vertext[2];
+    int weight;
+    Edge(int a, int b, int weight)
     {
-        this->node[0]=a;
-        this->node[1]=b;
-        this->distance=distance;
+        this->vertext[0]=a;
+        this->vertext[1]=b;
+        this->weight=weight;
     }
     bool operator <(Edge &edge)
     {
-        return this->distance < edge.distance;
+        return this->weight < edge.weight;
     }
 };
 
 int main()
 {
-    int n=8;
-    int m=14;
     vector<Edge> v;
     v.push_back(Edge(1,2,8));
     v.push_back(Edge(1,6,10));
@@ -59,24 +58,24 @@ int main()
     v.push_back(Edge(3,4,3));
     v.push_back(Edge(4,5,1));
     v.push_back(Edge(4,6,6));
-    v.push_back(Edge(4,7,3));
+    v.push_back(Edge(4,7,2));
     v.push_back(Edge(5,7,3));
     v.push_back(Edge(7,8,3));
 
     sort(v.begin(),v.end());
 
-    int set[8];
-    for(int i=0; i<n; i++) set[i]=i;
+    int cycledetection[SZ];
+    for(int i=0; i<SZ; i++) cycledetection[i]=i;
 
     int sum=0;
     for(int i=0; i<v.size(); i++)
     {
-        if(!find(set,v[i].node[0]-1,v[i].node[1]-1))
+        if(!find(cycledetection,v[i].vertext[0]-1,v[i].vertext[1]-1))
         {
-            sum+=v[i].distance;
-            unionparent(set,v[i].node[0]-1,v[i].node[1]-1);
+            sum+=v[i].weight;
+            unionparent(cycledetection,v[i].vertext[0]-1,v[i].vertext[1]-1);
         }
     }
 
-    cout<<sum;
+    cout<<"weight is : "<<sum;
 }
