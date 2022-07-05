9f92ed94efd662f87c06a8318fc1d925702bdc53
문예진 <dpwls991226@naver.com>
2022-07-05 21:02:19 +0900

    Problem 10610 - 30(greedy)

---
+++ C++/Baekjoon/Problem10610.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    sort(s.rbegin(),s.rend());
+    int len = s.length();
+    if(s[len-1]!='0') cout<<"-1";
+    else
+    {
+        long long num=0;
+        for(int i=0; i<len; i++)
+        {
+            num+=s[i]-'0';
+        }
+        if(num%3==0) cout<<s;
+        else cout<<"-1";
+    }
+}
