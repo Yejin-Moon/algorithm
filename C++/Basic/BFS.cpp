75e153c84fbaf8538d40d0f97b7e208be8001940
문예진 <dpwls991226@naver.com>
2022-07-26 22:14:42 +0900

    BFS로 최단경로 구하기

--- C++/Basic/BFS.cpp
+++ C++/Basic/BFS.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int graph[100][100];
+int graph[100][100], dist[100];
 int queue[100], front, rear,N;
 
 void bfs(int node)
 {
     bool visited[100] = {false};
+    dist[node] = 0;
     front = rear = -1;
     visited[node] = true;
     queue[++rear] = node;
@@ -19,6 +20,7 @@ void bfs(int node)
             if(!visited[next]&&graph[cur][next])
             {
                 visited[next] = true;
+                dist[next] = dist[cur]+1;
                 queue[++rear] = next;
             }
         }
