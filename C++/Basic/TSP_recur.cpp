27d71d70431fac8e4d543438ee6a2a16c3d812ce
문예진 <dpwls991226@naver.com>
2022-08-01 19:45:11 +0900

    TSP - recursion

---
+++ C++/Basic/TSP_recur.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+#define INF 987654321
+#define MAX_N 6
+int N, Graph[MAX_N][MAX_N];
+int solve(int pos, int visited);
+
+int main()
+{
+    int tcCnt;
+    cin>>tcCnt;
+
+    for(int t=1; t<=tcCnt; ++t)
+    {
+        cin>>N;
+        for(int i=0; i<N; ++i)
+        {
+            for(int j=0; j<N; ++j)
+            cin>>Graph[i][j];
+        }
+        int ans = INF;
+        for(int i=0; i<N; ++i)
+        {
+            int tmp = solve(i,1<<i);
+            if(ans>tmp) ans = tmp;
+        }
+        cout<<"#"<<t<<' '<<ans<<endl;
+    }
+    return 0;
+}
+
+int solve(int pos, int visited)
+{
+    if(visited==(1<<N)-1) return 0;
+    int ret = INF;
+    for(int next = 0; next<N; ++next)
+    {
+        if(!(visited&(1<<next))&&Graph[pos][next])
+        {
+            int tmp = Graph[pos][next] + solve(next,visited | (1<<next));
+            if(tmp<ret) ret = tmp;
+        }
+    }
+    return ret;
+}
