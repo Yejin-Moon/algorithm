13dfde63c1edb7119b947705294892176ee40ff3
문예진 <dpwls991226@naver.com>
2022-08-08 11:49:11 +0900

    Problem 12865 - 평범한 배낭(DP)

---
+++ C++/Baekjoon/Problem12865.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int maxval(int a, int b)
+{
+    if(a>b) return a;
+    else return b;
+}
+
+int main()
+{
+    int P[101][100001];
+    int weight[101];
+    int value[101];
+    int N,K;
+    cin>>N>>K;
+    for(int i=1; i<=N; i++)
+    {
+        cin>>weight[i]>>value[i];
+    }
+
+    for(int i=1; i<=N; i++)
+    {
+        for(int j=1; j<=K; j++)
+        {
+            if(weight[i]>j) P[i][j]=P[i-1][j];
+            else P[i][j]=maxval(value[i]+P[i-1][j-weight[i]],P[i-1][j]);
+        }
+    }
+    cout<<P[N][K];
+}
