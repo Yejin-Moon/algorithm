6d107307223e0c498a1ba3102163e578ed33be08
문예진 <dpwls991226@naver.com>
2022-08-24 22:09:46 +0900

    Problem 10808 - 알파벳 개수(string)

---
+++ C++/Baekjoon/Problem10808.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int arr[26];
+
+int main()
+{
+    char str[101];
+    scanf("%s",str);
+    for(int i=0; str[i]!=0; i++) arr[str[i]-'a']++;
+    for(int i=0; i<26; i++) printf("%d ",arr[i]);
+}
