6956725981d7c10f5b212ef76b5e6ea8fe7f7306
문예진 <dpwls991226@naver.com>
2022-09-01 22:00:42 +0900

    Problem 3273 - 두수의합(sort)

---
+++ C++/Baekjoon/Problem3273.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[100001];
+
+int main()
+{
+    int n,cnt=0;
+    scanf("%d",&n);
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int x;
+    scanf("%d",&x);
+    sort(arr,arr+n);
+    for(int i=0; i<n-1; i++)
+    {
+        if(arr[i]+arr[i+1]>x) break;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]+arr[j]==x) 
+            {
+                cnt++;
+                break;
+            }
+        }
+    }
+    printf("%d",cnt);
+}
