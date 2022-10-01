554e6ed28b453831bd2aac132970ee82085645d7
문예진 <dpwls991226@naver.com>
2022-10-01 23:37:50 +0900

    Problem 2525 - 오븐시계(math)

--- C++/Baekjoon/Problem2525.cpp
+++ C++/Baekjoon/Problem2525.cpp
@@ -9,8 +9,9 @@ int main()
     int h=C/60;
     int m=C%60;
 
-    int rh = (A+h)%23;
+    int rh = (A+h)%24;
     rh+=(B+m)/60;
+    if(rh==24) rh=0;
     int rm = (B+m)%60;
 
     cout<<rh<<" "<<rm;
