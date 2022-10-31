91bdd06d597ae8a8de416bce9837db25c5b9073d
문예진 <dpwls991226@naver.com>
2022-10-31 23:14:38 +0900

    Problem 1247

---
+++ C++/Baekjoon/Problem1247.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#define max 9223372036854775807
+#define min -9223372036854775808
+using namespace std;
+
+int main()
+{
+    long long res=0,flag=0;
+    for(int i=0; i<3; i++)
+    {
+        int n;
+        cin>>n;
+        res=0;
+        flag=0;
+        for(int j=0; j<n; j++)
+        {
+            long long a;
+            cin>>a;
+
+            if(res>0 && a>0 && a>max-res) flag+=1;
+            if(res<0 && a<0 && a<min-res) flag-=1;
+            res+=a;
+        }
+    }
+    if(flag<0) cout<<"-\n";
+    else if(flag>0) cout<<"+\n";
+    else if(res==0) cout<<"0\n";
+    else if(res<0) cout<<"-\n";
+    else if(res>0) cout<<"+\n";
+}
