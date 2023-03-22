53be789b50c8f2d8bf630ccfd91373dfdc05d619
문예진 <dpwls991226@naver.com>
2023-03-22 22:04:32 +0900

    Problem 25372

---
+++ C++/Baekjoon/Problem25372.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        string s;
+        cin>>s;
+        int len = s.length();
+        if(len>=6 && len<=9) cout<<"yes\n";
+        else cout<<"no\n";
+    }
+}
