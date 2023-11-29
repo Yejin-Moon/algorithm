1a7874002815f7a7083a964e717ab5f2fc19b80a
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2023-11-29 11:51:51 +0900

    Problem 1753 - 최단경로(다익스트라)

---
+++ C++/Baekjoon/Problem1753.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int inf = 987654321;
+
+vector<pair<int, int>> a[300001];
+int d[20001];
+
+void dijkstra(int start) {
+    d[start] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        int cur = pq.top().second;
+        int dis = pq.top().first;
+        pq.pop();
+
+        if (d[cur] < dis) continue;
+        for (int i = 0; i < a[cur].size(); i++) {
+            int next = a[cur][i].first;
+            int nextDis = dis + a[cur][i].second;
+
+            if (nextDis < d[next]) {
+                d[next] = nextDis;
+                pq.push({nextDis, next});
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int V, E, K;
+    cin >> V >> E >> K;
+
+    fill_n(d, 20001, inf);
+
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        a[u].push_back({v, w});
+    }
+    dijkstra(K);
+
+    for (int i = 1; i <= V; i++) {
+        if (d[i] == inf) cout << "INF\n";
+        else cout << d[i] << "\n";
+    }
+}
