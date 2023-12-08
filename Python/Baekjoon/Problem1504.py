8abb2b770f96c95616aefbc838ea2abacf8c2138
Yejin-Moon <74597602+Yejin-Moon@users.noreply.github.com>
2023-12-08 14:23:55 +0900

    Update Problem1504 - 특정한 최단 경로(성공 코드)

--- Python/Baekjoon/Problem1504.py
+++ Python/Baekjoon/Problem1504.py
@@ -1,6 +1,8 @@
 import heapq
 
+
 def dijkstra(start):
+    d = [inf for i in range(V + 1)]
     d[start] = 0
     h = []
     heapq.heappush(h, (0, start))
@@ -13,6 +15,7 @@ def dijkstra(start):
             if nextDis < d[i[0]]:
                 d[i[0]] = nextDis
                 heapq.heappush(h, (nextDis, i[0]))
+    return d
 
 
 V, E = map(int, input().split())
@@ -24,25 +27,24 @@ graph = [[] for i in range(V + 1)]
 for i in range(E):
     a, b, c = map(int, input().split())
     graph[a].append((b, c))
+    graph[b].append((a, c))
 
 v1, v2 = map(int, input().split())
 
-dijkstra(1)
-StartToV1 = d[v1]
-StartToV2 = d[v2]
+origin_dist = dijkstra(1)
+startToV1 = origin_dist[v1]
+startToV2 = origin_dist[v2]
 
-dijkstra(v1)
-V1ToEnd = d[V]
-V1ToV2 = d[v2]
+v1_dist = dijkstra(v1)
+v1ToEnd = v1_dist[V]
+v1ToV2 = v1_dist[v2]
 
-dijkstra(v2)
-V2ToEnd = d[V]
+v2_dist = dijkstra(v2)
+v2ToEnd = v2_dist[V]
 
-res = inf
-res = min(res, StartToV1 + V1ToV2 + V2ToEnd)
-res = min(res, StartToV2 + V1ToV2 + V1ToEnd)
+res = min(startToV1 + v1ToV2 + v2ToEnd, startToV2 + v1ToV2 + v1ToEnd)
 
-if res == inf or V1ToV2 == inf:
+if res >= inf:
     print(-1)
 else:
     print(res)
